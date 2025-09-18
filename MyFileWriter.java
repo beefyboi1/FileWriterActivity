d9bbdbb546a566d13ded853e5f1fedc394ecba1a
beef <anorden1@hwemail.com>
2025-09-18 15:13:20 -0700

    (MFW-0.5.2): Added a Test for HashFile
    tested the hashfile with multiple edge cases. it doesnt fully work yet but me and mr lopez couldnt figure it out

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MyFileWriter {
 
@@ -44,24 +48,34 @@ public static String stringify(String filePath) throws IOException {
 
 
 
-public static String hashFile(String path){
-    StringBuilder input = new StringBuilder();
+public static String hashFile(String path) {
+    //System.out.println("hello");
+
+    // debugger doenst work
+    // File file = new File(path);
+    // if (file.exists()){
+    //     throw new FileNotFoundException("file does not exist. double check file path");
+    // }
+    String input = "";
      try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = br.readLine()) != null) {
-                input.append(line);
+                input = input + line;
             }
         }
+        catch(IOException e){}
 
 
     // Static getInstance method is called with hashing SHA
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        
+        
 
         // digest() method called
         // to calculate message digest of an input
         // and return array of byte
         try{
-        byte[] hash =  md.digest(input.getBytes(StandardCharsets.UTF_8));
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hash =  md.digest(input.getBytes("UTF-8"));
 
         // Convert byte array into signum representation
         BigInteger number = new BigInteger(1, hash);
@@ -77,74 +91,84 @@ public static String hashFile(String path){
 
         return hexString.toString();
     }
-    catch(IOException e){
+    catch(Exception e){
         System.out.println("error of type: error");
     }
-        
+    return null;
 }
 
 
     public static void main(String[] args) {
-        String data = "Hello, World!";
-        String fileName1 = "example.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
-        String fileName6 = ".secretpassword.txt";
-        String fileName7 = "noLookie.txt";
+        // first tested it with the text "help" and it worked
+        // I then tried an empty string test and it worked
+        // afterwards i tried a file that doesnt exist and i had to add a thrown filenotfoundexception
+        // but with this fix it now didnt work at all and me and mr lopez could not figure it out
+        // so i took away the exception and it worked again
+        // finally i tested it with the bee movie script and it ran quickly
+        System.out.println(MyFileWriter.hashFile("test.txt"));
 
-        MyFileWriter.printFileSize("\\\\wsl.localhost\\Ubuntu-20.04\\home\\beef\\FileWriterActivity\\sizer.txt");
+
+
+        // String data = "Hello, World!";
+        // String fileName1 = "example.txt";
+        // String fileName2 = "example2.txt";
+        // String fileName3 = "example3.txt";
+        // String fileName4 = "example4.txt";
+        // String fileName5 = "example5.txt";
+        // String fileName6 = ".secretpassword.txt";
+        // String fileName7 = "noLookie.txt";
+
+        // MyFileWriter.printFileSize("\\\\wsl.localhost\\Ubuntu-20.04\\home\\beef\\FileWriterActivity\\sizer.txt");
 
        
 
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // // 1. Using FileWriter
+        // try (FileWriter writer = new FileWriter(fileName1)) {
+        //     writer.write(data);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // // 2. Using BufferedWriter
+        // try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
+        //     bufferedWriter.write(data);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName6))) {
-            bufferedWriter.write("a password");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName6))) {
+        //     bufferedWriter.write("a password");
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
-           try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName7))) {
-            bufferedWriter.write("confidential information");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //    try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName7))) {
+        //     bufferedWriter.write("confidential information");
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // // 3. Using FileOutputStream
+        // try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
+        //     outputStream.write(data.getBytes());
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // // 4. Using BufferedOutputStream
+        // try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
+        //     bufferedOutputStream.write(data.getBytes());
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
 
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        MyFileWriter.printFileSize("\\\\wsl.localhost\\Ubuntu-20.04\\home\\beef\\FileWriterActivity\\sizer.txt");
+        // // 5. Using Files (java.nio.file)
+        // try {
+        //     Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+        // MyFileWriter.printFileSize("\\\\wsl.localhost\\Ubuntu-20.04\\home\\beef\\FileWriterActivity\\sizer.txt");
 
     }
 }
